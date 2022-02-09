771db390d64b1eddae74524613e06b383b1a2ced
Afe <50844661+afwerki@users.noreply.github.com>
2022-02-09 22:19:03 +0000

    Add files via upload

---
+++ admin/includes/sidebarmenu.php
@@ -0,0 +1,28 @@
+<div class="sidebar-menu">
+					<header class="logo1">
+						<a href="#" class="sidebar-icon"> <span class="fa fa-bars"></span> </a> 
+					</header>
+						<div style="border-top:1px ridge rgba(255, 255, 255, 0.15)"></div>
+                           <div class="menu">
+									<ul id="menu" >
+										<li><a href="dashboard.php"><i class="fa fa-tachometer"></i> <span>Dashboard</span><div class="clearfix"></div></a></li>
+										
+									 <!--<li id="menu-academico" ><a href="#"><i class="fa fa-list-ul" aria-hidden="true"></i><span> Tour Packages</span> <span class="fa fa-angle-right" style="float: right"></span><div class="clearfix"></div></a>
+										   <ul id="menu-academico-sub" >
+											<li id="menu-academico-avaliacoes" ><a href="manage-packages.php">Manage</a></li>
+										  </ul>
+										</li>-->
+									<li id="menu-academico" ><a href="manage-users.php"><i class="fa fa-users" aria-hidden="true"></i><span>Manage Users</span><div class="clearfix"></div></a></li>
+
+									<!--<li id="menu-academico" ><a href="manage-users.php"><i class="fa fa-users" aria-hidden="true"></i><span>Manage admin</span><div class="clearfix"></div></a></li>-->
+									
+									<li><a href="mathsdoc.php"><i class="fa fa-list" aria-hidden="true"></i>  <span>Manage Maths Docs</span><div class="clearfix"></div></a></li>
+									 <li><a href="natural.php"><i class="fa fa-table"></i>  <span>Manage Natural scince Docs</span><div class="clearfix"></div></a></li>
+									<li><a href="tech.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>  <span>Manage Technology Docs</span><div class="clearfix"></div></a></li>
+									<li><a href="social.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>  <span>Manage Social scince Docs</span><div class="clearfix"></div></a></li>
+									<li><a href="admin.php"><i class="fa fa-file-text-o" aria-hidden="true"></i>  <span>manage admin</span><div class="clearfix"></div></a></li>
+							     
+									
+								  </ul>
+								</div>
+							  </div>
